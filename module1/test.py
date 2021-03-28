f57c342316085a4f33b0bb76a279113bb8c2facc
Chinedu Oty <otychinedu@gmail.com>
2021-03-28 17:31:40 +0100

    Completed module 1

--- module1/test.py
+++ module1/test.py
@@ -8,3 +8,49 @@ print(result)
 
 sq_odd = [x*x for x in range(20) if x%2!=0]
 print(sq_odd)
+
+print(((not False) and False) or (not (1 == 0)))
+
+def format_string(msg, prefix = 'string: ', postfix) -> int:
+    print(prefix + msg + postfix)
+
+format_string('hello', postfix='.')
+
+list = ['fourteen', 'fifteen', 16, 'seventeen']
+
+for item in list:
+    try:
+        msg = 'processing ' + item
+        print(msg)
+        continue
+    except TypeError:
+        print('something went wrong!')
+        break
+    finally:
+        print('finally block')
+    print('next item please!')
+
+def run_apple_guessing_game(target):
+    while True:
+        guess = int(input('Guess How Many Apples I Have!'))
+        if guess < target:
+            print('Too Few!')
+            continue
+        elif guess > target:
+            print('Too Many!')
+            continue
+        else:
+            print('You\'re Right!')
+            return
+
+run_apple_guessing_game(5)
+
+while True:
+    username = input('What is your username?')
+    if username != 'admin':
+        continue
+    password = input('What is your password?')
+    if password == 'topsecret':
+        break
+
+print('Authorised')
